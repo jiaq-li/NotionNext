b4d74359efa275d2d1378f334627a5ffb37190c9
RocheL <75050614+jiaq-li@users.noreply.github.com>
2022-08-02 12:23:52 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -2,7 +2,7 @@
 const BLOG = {
   AUTHOR: 'RocheL', // 作者
   BIO: '炼丹学徒👨‍💻', // 作者简介
-  LINK: 'https://tangly1024.com', // 网站地址
+  LINK: 'https://lijiaqi.vercel.app', // 网站地址
   KEYWORDS: 'Notion, 博客', // 网站关键词 英文逗号隔开
   NOTION_PAGE_ID:
   process.env.NOTION_PAGE_ID || '02ab3b8678004aa69e9e415905ef32a5', // Important page_id！！！Duplicate Template from  https://www.notion.so/tanghh/02ab3b8678004aa69e9e415905ef32a5
